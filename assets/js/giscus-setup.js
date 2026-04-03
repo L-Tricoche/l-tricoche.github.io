29bb39c0ad7deab96b8073877b4bdb433b6c7e29
L-Tricoche <L-Tricoche@users.noreply.github.com>
2026-04-03 14:43:46 +0000

    Deploying to gh-pages from @ L-Tricoche/l-tricoche.github.io@e6e1e9f648403c4d23707c016489e143bcd34b8a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "L-Tricoche/leslietricoche.github.io",
+    "data-repo": "L-Tricoche/l-tricoche.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
