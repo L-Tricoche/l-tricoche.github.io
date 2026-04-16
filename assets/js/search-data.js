77ac748650597739a1a32f4fc4ad999b2aa89830
L-Tricoche <leslie.tricoche@ecomail.fr>
2026-04-16 14:29:05 +0000

    Deploying to gh-pages from @ L-Tricoche/l-tricoche.github.io@e9ac9020a7da0b539df02bbb10159ccd1636f34b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -421,9 +421,9 @@ ninja.data = [{
           section: "News",},{id: "news-work-in-progress-news-sparkles-smile",
           title: 'WORK IN PROGRESS (_news) :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-ecosoc-project",
+          title: 'ECOSOC project',
+          description: "in collaboration with Emilie Caspar, Damien Brevers and Claus Lamm",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
